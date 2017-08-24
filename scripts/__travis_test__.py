636f853a324ffc38f9ebe26ff1489bc2631d26ea
Ali Razmjoo <ali.razmjoo@owasp.org>
2017-08-25 02:25:31 +0430

    add 0.1 timeout

--- scripts/__travis_test__.py
+++ scripts/__travis_test__.py
@@ -4,4 +4,4 @@
 import os
 
 os.system('python nettacker.py --help')
-os.system("python nettacker.py -i 127.0.0.1 -u user1,user2 -p pass1,pass2 -m all -g 21,25,80,443 -t 2")
+os.system("python nettacker.py -i 127.0.0.1 -u user1,user2 -p pass1,pass2 -m all -g 21,25,80,443 -t 2 -T 0.1")
