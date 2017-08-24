80451e73c2ff46aa2a0c98f006ba86b31bbbfddc
Ali Razmjoo <ali.razmjoo@owasp.org>
2017-08-24 20:28:26 +0430

    simple bug fix

--- core/load_modules.py
+++ core/load_modules.py
@@ -9,11 +9,11 @@ def load_all_modules():
     # Search for Modules
     module_names = []
     for lib in glob('lib/brute/*/engine.py'):
-        ib = lib.rsplit('\\' if sys.platform == 'win32' or sys.platform == 'win64' else '/')[-2]
+        lib = lib.rsplit('\\' if sys.platform == 'win32' or sys.platform == 'win64' else '/')[-2]
         if lib + '_brute' not in module_names:
             module_names.append(lib + '_brute')
     for lib in glob('lib/scan/*/engine.py'):
-        ib = lib.rsplit('\\' if sys.platform == 'win32' or sys.platform == 'win64' else '/')[-2]
+        lib = lib.rsplit('\\' if sys.platform == 'win32' or sys.platform == 'win64' else '/')[-2]
         if lib + '_scan' not in module_names:
             module_names.append(lib + '_scan')
     module_names.append('all')
