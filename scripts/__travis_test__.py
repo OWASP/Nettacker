a857bcaed27eec9909968068149af30804488533
Ali Razmjoo <ali.razmjoo@owasp.org>
2017-08-24 19:51:32 +0430

    add travis ci script for testing all functions

---
+++ scripts/__travis_test__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+os.system('python nettacker.py --help')
+os.system("python nettacker.py -i 127.0.0.1 -u user1,user2 -p pass1,pass2 -m all -g 21,25,80,443 -t 2")
