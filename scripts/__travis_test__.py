d62bc64db740b8467aeabb5a88bd46503c0bcd31
Ali Razmjoo <ali.razmjoo@owasp.org>
2017-08-27 16:48:17 +0430

    add time command, run twice for testing cache speed

--- scripts/__travis_test__.py
+++ scripts/__travis_test__.py
@@ -3,5 +3,11 @@
 
 import os
 
-os.system('python nettacker.py --help')
-os.system("python nettacker.py -i 127.0.0.1 -u user1,user2 -p pass1,pass2 -m all -g 21,25,80,443 -t 2 -T 0.1")
+# Test help menu command
+os.system('time python nettacker.py --help')
+# Test show version command
+os.system('time python nettacker.py --version')
+# Test all modules command
+os.system("time python nettacker.py -i 127.0.0.1 -u user1,user2 -p pass1,pass2 -m all -g 21,25,80,443 -t 2 -T 0.1")
+# Test all modules for second time (testing cache) command
+os.system("time python nettacker.py -i 127.0.0.1 -u user1,user2 -p pass1,pass2 -m all -g 21,25,80,443 -t 2 -T 0.1")
