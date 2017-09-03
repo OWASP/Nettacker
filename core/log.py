7497f2a71cb941407defe2c34910aa06b6be0786
Ali Razmjoo <ali.razmjoo@owasp.org>
2017-09-03 20:24:03 +0430

    replace d3 library with jit

--- core/log.py
+++ core/log.py
@@ -113,16 +113,69 @@ def build_graph(language, data, _HOST, _USERNAME, _PASSWORD, _PORT, _TYPE, _DESC
                     if add_flag:
                         dgraph["children"][b]["children"][d]["children"].append(_to_modify)
 
-    return open('lib/jit/sample.html').read() \
-        .replace('__data_will_locate_here__', json.dumps(dgraph)) \
-        .replace('__js_jit_lib_will_locate_here__', open('lib/jit/jit-yc.js').read()) \
+    # dgraph until here is compatible with jit library
+    # start making it to be compatible to d3 library
+    d3_structure = {
+        "name": "Started Attack",
+        "children": [],
+    }
+    for d3_level1 in dgraph["children"]:
+        for d3_level2 in d3_level1:
+            _to_modify = {
+                "name": d3_level1["name"],
+                "children": []
+            }
+            add_flag = True
+            for d3_name in d3_structure["children"]:
+                if _to_modify["name"] == d3_name["name"]:
+                    add_flag = False
+            if add_flag:
+                d3_structure["children"].append(_to_modify)
+    for i in range(0, len(dgraph["children"])):
+        for j in range(0, len(dgraph["children"][i]["children"])):
+            ip = dgraph["children"][i]["name"]
+            method = dgraph["children"][i]["children"][j]["name"]
+            for k in range(0, len(d3_structure["children"])):
+                if ip == d3_structure["children"][k]["name"]:
+                    add_flag = True
+                    for l in range(0, len(d3_structure["children"][k]["children"])):
+                        if d3_structure["children"][k]["children"][l]["name"] == method:
+                            add_flag = False
+                    _to_modify = {
+                        "name": method,
+                        "children": []
+                    }
+                    if add_flag:
+                        d3_structure["children"][k]["children"].append(_to_modify)
+
+    for i in range(0, len(dgraph["children"])):
+        for j in range(0, len(dgraph["children"][i]["children"])):
+            for k in range(0, len(dgraph["children"])):
+                ip = dgraph["children"][k]["name"]
+                for d3_level3 in dgraph["children"][k]["children"]:
+                    for l in range(0, len(dgraph["children"][k]["children"])):
+                        method = dgraph["children"][k]["children"][l]["name"]
+                        for m in range(0, len(dgraph["children"][k]["children"][l]["children"])):
+                            port = dgraph["children"][k]["children"][l]["children"][m]["data"]["relation"][0][0]
+                            description = dgraph["children"][k]["children"][l]["children"][m]["data"]["relation"][0][1]
+                            user = dgraph["children"][k]["children"][l]["children"][m]["data"]["relation"][0][2]
+                            passwd = dgraph["children"][k]["children"][l]["children"][m]["data"]["relation"][0][3]
+                            desc = _HOST + ': ' + ip + ' ' + port + ' ' + description + ' ' + user + ' ' + passwd
+                            _to_modify = {
+                                "name": desc,
+                            }
+                            add_flag = True
+                            for n in range(0, len(d3_structure["children"][k]["children"][l]["children"])):
+                                if d3_structure["children"][k]["children"][l]["children"][n]["name"] == desc:
+                                    add_flag = False
+                            if add_flag:
+                                d3_structure["children"][k]["children"][l]["children"].append(_to_modify)
+
+    return open('lib/d3/sample.html','rb').read().decode('utf8') \
+        .replace('__data_will_locate_here__', json.dumps(d3_structure))\
         .replace('__title_to_replace__', messages(language, 90)) \
         .replace('__description_to_replace__', messages(language, 91)) \
-        .replace('__html_title_to_replace__', messages(language, 92)) \
-        .replace('__time_and_version_to_replace__', messages(language, 93)
-                 .format(compatible.__version__,
-                         compatible.__code_name__,
-                         datetime.datetime.now()))
+        .replace('__html_title_to_replace__', messages(language, 92))
 
 
 def sort_logs(log_in_file, language, graph_flag):
