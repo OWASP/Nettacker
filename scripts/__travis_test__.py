b2bb62a1be00d7fbb0e6eef4541c985e5b46cefe
Ali Razmjoo <ali.razmjoo@owasp.org>
2017-09-07 02:34:34 +0430

    update travisci script

--- scripts/__travis_test__.py
+++ scripts/__travis_test__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 # System info
 os.system('free -m')
@@ -11,7 +12,10 @@ os.system('cat /proc/cpuinfo')
 os.system('time python nettacker.py --help')
 # Test show version command
 os.system('time python nettacker.py --version')
-# Test all modules command
-os.system("time python nettacker.py -i 127.0.0.1 -u user1,user2 -p pass1,pass2 -m all -g 21,25,80,443 -t 2 -T 0.1")
+# Test all modules command + check if it's finish successfully
+if os.system("time python nettacker.py -i 127.0.0.1 -u user1,user2 -p "
+             "pass1,pass2 -m all -g 21,25,80,443 -t 2 -T 0.1") is not 0:
+    sys.exit(1)
+
 # Test all modules for second time (testing cache) command
 os.system("time python nettacker.py -i 127.0.0.1 -u user1,user2 -p pass1,pass2 -m all -g 21,25,80,443 -t 2 -T 0.1")
