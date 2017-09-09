a6b8779612ffb1bf0c5faa1f1074793b93d39407
Ali Razmjoo <ali.razmjoo@owasp.org>
2017-09-09 04:56:03 +0430

    remove junk codes

--- core/alert.py
+++ core/alert.py
@@ -46,38 +46,20 @@ def write(content):
 
 
 def warn(content):
-    if '\n' in content:
-        num_newline = len(content) - len(content.rstrip("\n"))
-        if version() is 2:
-            sys.stdout.write(color.color('blue') + '[!] ' + color.color('yellow') +
-                             content[:-num_newline].encode('utf8') + color.color('reset') + "\n" * num_newline)
-        else:
-            sys.stdout.buffer.write(bytes(color.color('blue') + '[!] ' + color.color('yellow') +
-                                          content[:-num_newline] + color.color('reset') + "\n" * num_newline), 'utf8')
+    if version() is 2:
+        sys.stdout.write(color.color('blue') + '[!] ' + color.color('yellow') +
+                         content.encode('utf8') + color.color('reset') + "\n")
     else:
-        if version() is 2:
-            sys.stdout.write(color.color('blue') + '[!] ' + color.color('yellow') +
-                             content.encode('utf8') + color.color('reset') + "\n")
-        else:
-            sys.stdout.buffer.write(bytes(color.color('blue') + '[!] ' + color.color('yellow') +
-                                          content + color.color('reset') + "\n"), 'utf8')
+        sys.stdout.buffer.write(bytes(color.color('blue') + '[!] ' + color.color('yellow') +
+                                      content + color.color('reset') + "\n"), 'utf8')
     return
 
 
 def error(content):
-    if '\n' in content:
-        num_newline = len(content) - len(content.rstrip("\n"))
-        if version() is 2:
-            sys.stdout.write(color.color('red') + '[X] ' + color.color('yellow') +
-                             content[:-num_newline].encode('utf8') + color.color('reset') + "\n" * num_newline)
-        else:
-            sys.stdout.buffer.write(bytes(color.color('red') + '[X] ' + color.color('yellow') +
-                                          content[:-num_newline] + color.color('reset') + "\n" * num_newline), 'utf8')
+    if version() is 2:
+        sys.stdout.write(color.color('red') + '[X] ' + color.color('yellow') +
+                         content.encode('utf8') + color.color('reset') + "\n")
     else:
-        if version() is 2:
-            sys.stdout.write(color.color('red') + '[X] ' + color.color('yellow') +
-                             content.encode('utf8') + color.color('reset') + "\n")
-        else:
-            data = color.color('red') + '[X] ' + color.color('yellow') + content + color.color('reset') + "\n"
-            sys.stdout.buffer.write(data.encode('utf8'))
+        data = color.color('red') + '[X] ' + color.color('yellow') + content + color.color('reset') + "\n"
+        sys.stdout.buffer.write(data.encode('utf8'))
     return
