d24dca1a226a994b4675361c9a04dc023d85898d
Ali Razmjoo <ali.razmjoo@owasp.org>
2017-10-18 17:19:52 +0330

    fix update function

--- core/update.py
+++ core/update.py
@@ -15,13 +15,17 @@ def _update(__version__, __code_name__, language):
         from urllib import urlopen
     if version() is 3:
         from urllib.request import urlopen
-    data = urlopen(url).read()
-    if version() is 3:
-        data = data.decode("utf-8")
-    if __version__ + ' ' + __code_name__ == str(data.rsplit('\n')[0]):
-        info(messages(language, 103))
-    else:
-        warn(messages(language, 85))
+    try:
+        data = urlopen(url).read()
+        if version() is 3:
+            data = data.decode("utf-8")
+        if __version__ + ' ' + __code_name__ == data.rsplit('\n')[0]:
+            info(messages(language, 103))
+        else:
+            warn(messages(language, 101))
+            warn(messages(language, 85))
+    except:
+        warn(messages(language, 102))
     return
 
 
