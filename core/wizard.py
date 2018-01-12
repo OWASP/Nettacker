7255f5e2a2f6ffd4aa87225287e7b1666b0f2bb8
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-01-12 16:57:24 +0330

    create wizard function

--- core/wizard.py
+++ core/wizard.py
@@ -18,37 +18,38 @@ def __wizard(targets, thread_number, thread_number_host,
     ex_module_name = scan_method[:]
     ex_module_name.remove('all')
     default_config = _builder(get_config())
-    targets = __input(messages(language, 118).format('the targets', default_config["targets"]),
+    targets = __input(messages(language, 118).format(messages(language, 120), default_config["targets"]),
                       default_config["targets"])
-    thread_number = __input(messages(language, 118).format('the thread number', default_config["thread_number"]),
+    thread_number = __input(messages(language, 118).format(messages(language, 121), default_config["thread_number"]),
                             default_config["thread_number"])
     thread_number_host = __input(messages(language, 118)
-                                 .format('the thread numbers for scan hosts', default_config["thread_number_host"]),
+                                 .format(messages(language, 122), default_config["thread_number_host"]),
                                  default_config["thread_number_host"])
-    log_in_file = __input(messages(language, 118).format('the output filename', default_config["log_in_file"]),
+    log_in_file = __input(messages(language, 118).format(messages(language, 123), default_config["log_in_file"]),
                           default_config["log_in_file"])
     scan_method = __input(messages(language, 119)
-                          .format('the scan methods', ', '.join(module_name), default_config["scan_method"]),
+                          .format(messages(language, 124), ', '.join(module_name), default_config["scan_method"]),
                           default_config["scan_method"])
     exclude_method = __input(messages(language, 119)
-                             .format('the scan methods to exclude', ', '.join(ex_module_name),
+                             .format(messages(language, 125), ', '.join(ex_module_name),
                                      default_config["exclude_method"]),
                              default_config["exclude_method"])
-    users = __input(messages(language, 118).format('the usernames', default_config["users"]), default_config["users"])
-    passwds = __input(messages(language, 118).format('the passwords', default_config["passwds"]),
+    users = __input(messages(language, 118).format(messages(language, 126), default_config["users"]),
+                    default_config["users"])
+    passwds = __input(messages(language, 118).format(messages(language, 127), default_config["passwds"]),
                       default_config["passwds"])
-    timeout_sec = __input(messages(language, 118).format('the timeout seconds', default_config["timeout_sec"]),
+    timeout_sec = __input(messages(language, 118).format(messages(language, 128), default_config["timeout_sec"]),
                           default_config["timeout_sec"])
-    ports = __input(messages(language, 118).format('the port numbers', default_config["ports"]),
+    ports = __input(messages(language, 118).format(messages(language, 129), default_config["ports"]),
                     default_config["ports"])
-    verbose_level = __input(messages(language, 118).format('the verbose level', default_config["verbose_level"]),
+    verbose_level = __input(messages(language, 118).format(messages(language, 130), default_config["verbose_level"]),
                             default_config["verbose_level"])
-    socks_proxy = __input(messages(language, 118).format('the socks proxy', default_config["socks_proxy"]),
+    socks_proxy = __input(messages(language, 118).format(messages(language, 131), default_config["socks_proxy"]),
                           default_config["socks_proxy"])
-    retries = __input(messages(language, 118).format('the retries number', default_config["retries"]),
+    retries = __input(messages(language, 118).format(messages(language, 132), default_config["retries"]),
                       default_config["retries"])
     graph_flag = __input(messages(language, 119)
-                         .format('a graph', ', '.join(graph_flag), default_config["graph_flag"]),
+                         .format(messages(language, 133), ', '.join(graph_flag), default_config["graph_flag"]),
                          default_config["graph_flag"])
     return [targets, thread_number, thread_number_host,
             log_in_file, scan_method, exclude_method, users,
