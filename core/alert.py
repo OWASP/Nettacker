7ba7d62a6512aa19cb9db22f2d81fb480f721193
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-01-12 10:40:39 +0330

    pretending 3 last commits did never exists #10
    3 last commits works but It's using too many resources, and framework became x10 slower at least! it's bests to use python/json structure!

--- core/alert.py
+++ core/alert.py
@@ -2,26 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import os
-import glob
 import sys
 import time
 import random
 from core import color
-from core.languages import all_messages
 from core.compatible import version
 
 
 def messages(language, msg_id):
-    # Importing messages
-    msgs = all_messages(language)
-
     # Returning selected langauge
     if language is -1:
-        return [langs.rsplit('_')[1].rsplit('.')[0] for langs in
-                os.listdir(os.path.dirname(os.path.abspath(__file__)).replace('\\', '/') + '/../lib/language/') if
-                langs != 'readme.md']
-
-    return msgs['data']['msg_id_{0}'.format(msg_id)]['$'].replace('\\n','\n')
+        return list(set([langs.rsplit('_')[1].rsplit('.')[0] for langs in
+                         os.listdir(os.path.dirname(os.path.abspath(__file__)).replace('\\', '/') + '/../lib/language/')
+                         if
+                         langs != 'readme.md']))
+    all_messages = getattr(__import__('lib.language.messages_{0}'.format(language), fromlist=['all_messages']),
+                           'all_messages')
+    # Importing messages
+    msgs = all_messages()[str(msg_id)]
+    if version() is 2:
+        try:
+            return msgs.decode('utf8') if msgs != '' else \
+                getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
+                        'all_messages')()[str(msg_id)].decode('utf8')
+        except:
+            return getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
+                           'all_messages')()[str(msg_id)].decode('utf8')
+    try:
+        return msgs if msgs != '' else \
+            getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
+                    'all_messages')()[str(msg_id)]
+    except:
+        return getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
+                       'all_messages')()[str(msg_id)]
 
 
 def info(content):
