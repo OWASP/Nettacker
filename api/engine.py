83206217242ee7b9519e77cf1de5f66a1101e726
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-01-18 22:07:57 +0330

    update api engine #4

--- api/engine.py
+++ api/engine.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import multiprocessing
+import time
 from flask import Flask
-from core.config_builder import _builder
-from core.config_builder import _api_default_config
-from config import _api_config
+from core.alert import write
+from core.alert import messages
+from core._die import __die_success
 
 app = Flask(__name__)
 
@@ -14,6 +16,29 @@ def index():
     return "Hello, World!"
 
 
-if __name__ == '__main__':
-    config = _builder(_api_config(), _api_default_config())
-    app.run(host=config["host"], port=config["port"], debug=config["debug_mode"])
+def __process_it(api_host, api_port, api_debug_mode):
+    app.run(host=api_host, port=api_port, debug=api_debug_mode)
+
+
+def _start_api(api_host, api_port, api_debug_mode, api_access_key, api_client_white_list,
+               api_client_white_list_ips, api_access_log, api_access_log_filename, language):
+    # Starting the API
+    write(messages(language, 156).format(api_access_key))
+    p = multiprocessing.Process(target=__process_it, args=(api_host, api_port, api_debug_mode))
+    p.start()
+    # Sometimes it's take much time to terminate flask with CTRL+C
+    # So It's better to use KeyboardInterrupt to terminate!
+    while 1:
+        try:
+            exitflag = True
+            if len(multiprocessing.active_children()) is not 0:
+                exitflag = False
+            time.sleep(0.3)
+            if exitflag:
+                break
+        except KeyboardInterrupt:
+            for process in multiprocessing.active_children():
+                process.terminate()
+            break
+
+    __die_success()
