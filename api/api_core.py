30da9b530f5a07a63f5e2edff99091160c8adb89
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-01-24 19:05:53 +0330

    add a few rules

--- api/api_core.py
+++ api/api_core.py
@@ -5,6 +5,9 @@ from core.load_modules import load_all_modules
 from core.load_modules import load_all_graphs
 from core.alert import messages
 from core.alert import warn
+from core.config_builder import default_profiles
+from core.config import _profiles
+from core.config_builder import _builder
 
 
 def __structure(status="", msg=""):
@@ -38,5 +41,162 @@ def __remove_non_api_keys(config):
 
 
 def __rules(config, defaults, language):
-    
+    # Check Ranges
+    config["check_ranges"] = True if config["check_ranges"] is not False else False
+    # Check Subdomains
+    config["check_subdomains"] = True if config["check_subdomains"] is not False else False
+    # Check Graph
+    config["graph_flag"] = config["graph_flag"] if config["graph_flag"] in load_all_graphs() else None
+    # Check Language
+    config["language"] = config["language"] if config["language"] in [lang for lang in messages(-1, 0)] else "en"
+    # Check Log File
+    try:
+        f = open(config["log_in_file"], "a")
+        f.close()
+    except:
+        warn("file {0} is not writable! change results file to \"{1}\".".format(config["log_in_file"],
+                                                                                defaults["log_in_file"]))
+        config["log_in_file"] = defaults["log_in_file"]
+    # Check Method ARGS
+    methods_args = config["methods_args"]
+    if methods_args is not None:
+        new_methods_args = {}
+        methods_args = methods_args.rsplit("&")
+        for imethod_args in methods_args:
+            if len(imethod_args.rsplit("=")) is 2:
+                new_methods_args[imethod_args.rsplit("=")[0]] = imethod_args.rsplit("=")[1].rsplit(",")
+            else:
+                new_methods_args[imethod_args.rsplit("=")[0]] = ""
+        methods_args = new_methods_args
+    config["methods_args"] = methods_args[:]
+
+    # Check Passwords
+    config["passwds"] = config["passwds"].rsplit(',') if config["passwds"] is not None else None
+    # Check Ping Before Scan
+    config["ping_flag"] = True if config["ping_flag"] is not False else False
+    # Check Ports
+    ports = config["ports"]
+    if ports is not None:
+        tmp_ports = []
+        for port in ports.rsplit(','):
+            try:
+                if '-' not in port:
+                    if int(port) not in tmp_ports:
+                        tmp_ports.append(int(port))
+                else:
+                    t_ports = range(int(port.rsplit('-')[0], int(port.rsplit('-')[1]) + 1))
+                    for p in t_ports:
+                        if p not in tmp_ports:
+                            tmp_ports.append(p)
+            except:
+                pass
+        if len(tmp_ports) is 0:
+            ports = None
+        else:
+            ports = tmp_ports[:]
+    config["ports"] = ports
+    # Check Profiles
+    config["profile"] = config["profile"].rsplit(',') if config["profile"] is not None else None
+    if config["profile"] is not None:
+        if config["scan_method"] is None:
+            config["scan_method"] = ""
+        else:
+            config["scan_method"] += ","
+        if config["profile"][0] == "all":
+            config["profile"] = ",".join(_builder(_profiles(), default_profiles()))
+        tmp_sm = config["scan_method"]
+        for pr in config["profile"].rsplit(","):
+            try:
+                for sm in _builder(_profiles(), default_profiles())[pr]:
+                    if sm not in tmp_sm.rsplit(","):
+                        tmp_sm += sm + ","
+            except:
+                warn("profile \"{0}\" not found! skipping...".format(pr))
+        if tmp_sm[-1] == ",":
+            tmp_sm = tmp_sm[0:-1]
+        config["scan_method"] = ",".join(list(set(tmp_sm.rsplit(","))))
+    # Check retries
+    try:
+        config["retries"] = int(config["retries"])
+    except:
+        config["retries"] = defaults["retries"]
+    # Check Scanning Method
+    config["scan_method"] = config["scan_method"].rsplit(',') if config["scan_method"] is not None else None
+    if config["scan_method"] is None:
+        warn("you must select a scanning method, \"tcp_connect_port_scan\" selected by default!")
+        config["scan_method"] = ["tcp_connect_port_scan"]
+    else:
+        if "all" in config["scan_method"]:
+            config["scan_method"] = load_all_modules()
+            config["scan_method"].remove("all")
+        else:
+            methods = config["scan_method"][:]
+            for sm in methods:
+                if sm not in load_all_modules():
+                    warn("did not find \"{0}\", module removed from list!".format(sm))
+                    config["scan_method"].remove(sm)
+            if len(sm) is 0:
+                warn("your scan method became to zero! we select \"tcp_connect_port_scan\" by default!")
+                config["scan_method"] = ["tcp_connect_port_scan"]
+    # Check Socks Proxy
+    socks_proxy = config["socks_proxy"]
+    if socks_proxy is not None:
+        e = False
+        if socks_proxy.startswith("socks://"):
+            socks_flag = 5
+            socks_proxy = socks_proxy.replace("socks://", "")
+        elif socks_proxy.startswith("socks5://"):
+            socks_flag = 5
+            socks_proxy = socks_proxy.replace("socks5://", "")
+        elif socks_proxy.startswith("socks4://"):
+            socks_flag = 4
+            socks_proxy = socks_proxy.replace("socks4://", "")
+        else:
+            socks_flag = 5
+        if "://" in socks_proxy:
+            socks_proxy = socks_proxy.rsplit("://")[1].rsplit("/")[0]
+        try:
+            if len(socks_proxy.rsplit(":")) < 2 or len(socks_proxy.rsplit(":")) > 3:
+                e = True
+            elif len(socks_proxy.rsplit(":")) is 2 and socks_proxy.rsplit(":")[1] == "":
+                e = True
+            elif len(socks_proxy.rsplit(":")) is 3 and socks_proxy.rsplit(":")[2] == "":
+                e = True
+        except:
+            e = True
+        if e:
+            socks_proxy = None
+        if socks_flag is 4:
+            socks_proxy = "socks4://" + socks_proxy
+        if socks_flag is 5:
+            socks_proxy = "socks5://" + socks_proxy
+    config["socks_proxy"] = socks_proxy
+    # Check Targets
+    if config["targets"] is not None:
+        config["targets"] = list(set(config["targets"].rsplit(",")))
+    else:
+        warn("you must enter a target, set \"127.0.0.1\" by default!")
+        config["targets"] = ["127.0.0.1"]
+    # Check thread numbers
+    try:
+        config["thread_number"] = int(config["thread_number"])
+    except:
+        config["thread_number"] = defaults["thread_number"]
+    # Check thread number for hosts
+    try:
+        config["thread_number_host"] = int(config["thread_number_host"])
+    except:
+        config["thread_number_host"] = defaults["thread_number_host"]
+    # Check time sleep
+    try:
+        config["time_sleep"] = float(config["time_sleep"])
+    except:
+        config["time_sleep"] = defaults["time_sleep"]
+    # Check timeout sec
+    try:
+        config["timeout_sec"] = int(config["timeout_sec"])
+    except:
+        config["thread_number_host"] = defaults["thread_number_host"]
+    # Check users
+    config["users"] = config["users"].rsplit(',') if config["users"] is not None else None
     return config
