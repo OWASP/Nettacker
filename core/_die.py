706fadbab31f54961ceae1a57352c71e1ceb1d79
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-01-28 23:42:53 +0330

    make function private , bug fixed

--- core/_die.py
+++ core/_die.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from core.color import finish
 
 
 def __die_success():
+    from core.color import finish
     finish()
     sys.exit(0)
 
 
 def __die_failure(msg):
+    from core.color import finish
     from core.alert import error
     error(msg)
     finish()
