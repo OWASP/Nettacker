7fbe09aa0fd53857ea0dca5a7bcb2ddfa5a8fdfb
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-02-03 19:27:02 +0330

    fug fixed

--- core/alert.py
+++ core/alert.py
@@ -20,25 +20,17 @@ def messages(language, msg_id):
         return list(set([langs.rsplit('_')[1].rsplit('.')[0] for langs in
                          os.listdir(os.path.dirname(os.path.abspath(__file__)).replace('\\', '/') + '/../lib/language/')
                          if langs != 'readme.md' and langs.rsplit('_')[1].rsplit('.')[0] != '']))
-    all_messages = getattr(__import__('lib.language.messages_{0}'.format(language), fromlist=['all_messages']),
-                           'all_messages')
     # Importing messages
+    try:
+        all_messages = getattr(__import__('lib.language.messages_{0}'.format(language), fromlist=['all_messages']),
+                               'all_messages')
+    except:
+        all_messages = getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
+                               'all_messages')
     msgs = all_messages()[str(msg_id)]
     if version() is 2:
-        try:
-            return msgs.decode('utf8') if msgs != '' else \
-                getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
-                        'all_messages')()[str(msg_id)].decode('utf8')
-        except:
-            return getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
-                           'all_messages')()[str(msg_id)].decode('utf8')
-    try:
-        return msgs if msgs != '' else \
-            getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
-                    'all_messages')()[str(msg_id)]
-    except:
-        return getattr(__import__('lib.language.messages_en'.format(language), fromlist=['all_messages']),
-                       'all_messages')()[str(msg_id)]
+        return msgs.decode('utf8')
+    return msgs
 
 
 def __input_msg(content):
