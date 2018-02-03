9e3359cd18de33dcf6881e40fc23e275bd5dc653
Arian <16976612+ArianPH@users.noreply.github.com>
2018-02-04 01:12:40 +0330

    Update engine.py

--- lib/scan/dir/engine.py
+++ lib/scan/dir/engine.py
@@ -201,7 +201,7 @@ def start(target, users, passwds, ports, timeout_sec, thread_number, num, total,
         max = thread_number
         total_req = len(extra_requirements["dir_scan_list"]) * len(ports)
         filepath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-        thread_tmp_filename = '{}/tmp/thread_tmp_'.formaat(filepath) + ''.join(
+        thread_tmp_filename = '{}/../../tmp/thread_tmp_'.formaat(filepath) + ''.join(
             random.choice(string.ascii_letters + string.digits) for _ in range(20))
         __log_into_file(thread_tmp_filename, 'w', '1')
         trying = 0
