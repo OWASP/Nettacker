1b0a814148d4b8a4118d22e18db0d7f6fb1ed09e
Arian <16976612+ArianPH@users.noreply.github.com>
2018-02-04 01:05:12 +0330

    Update engine.py

--- lib/brute/smtp/engine.py
+++ lib/brute/smtp/engine.py
@@ -225,9 +225,10 @@ def start(target, users, passwds, ports, timeout_sec, thread_number, num, total,
             len(users) * len(passwds) * len(ports) * len(extra_requirements["smtp_brute_split_user_set_pass_prefix"])) \
             if extra_requirements["smtp_brute_split_user_set_pass"][0] == "False" \
             else int(len(users) * len(ports) * len(extra_requirements["smtp_brute_split_user_set_pass_prefix"]))
-        thread_tmp_filename = 'tmp/thread_tmp_' + ''.join(
+        filepath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        thread_tmp_filename = '{}/tmp/thread_tmp_'.format(filepath) + ''.join(
             random.choice(string.ascii_letters + string.digits) for _ in range(20))
-        ports_tmp_filename = 'tmp/ports_tmp_' + ''.join(
+        ports_tmp_filename = '{}/tmp/ports_tmp_'.format(filepath) + ''.join(
             random.choice(string.ascii_letters + string.digits) for _ in range(20))
         __log_into_file(thread_tmp_filename, 'w', '1')
         __log_into_file(ports_tmp_filename, 'w', '')
