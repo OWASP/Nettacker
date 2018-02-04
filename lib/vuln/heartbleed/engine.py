561f574617523e6eb5f56cfd06a7c67d28bf4cac
Arian <16976612+ArianPH@users.noreply.github.com>
2018-02-04 16:27:48 +0330

    Update engine.py

--- lib/vuln/heartbleed/engine.py
+++ lib/vuln/heartbleed/engine.py
@@ -19,6 +19,7 @@ import binascii
 from core.alert import *
 from core.targets import target_type
 from core.targets import target_to_host
+from core.load_modules import load_file_path
 from lib.icmp.engine import do_one as do_one_ping
 from lib.socks_resolver.engine import getaddrinfo
 from core._time import now
@@ -238,8 +239,7 @@ def start(target, users, passwds, ports, timeout_sec, thread_number, num, total,
         threads = []
         max = thread_number
         total_req = len(ports)
-        filepath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-        thread_tmp_filename = '{}/../../tmp/thread_tmp_'.format(filepath) + ''.join(
+        thread_tmp_filename = '{}/tmp/thread_tmp_'.format(load_file_path()) + ''.join(
             random.choice(string.ascii_letters + string.digits) for _ in range(20))
         __log_into_file(thread_tmp_filename, 'w', '1')
         trying = 0
