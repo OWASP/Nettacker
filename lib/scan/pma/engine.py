50bf560fd15eb008e99a525b6c952bb537de9d6a
shaddygarg <shaddygarg1@gmail.com>
2018-02-15 01:13:31 +0530

    Fix issue of break statements

--- lib/scan/pma/engine.py
+++ lib/scan/pma/engine.py
@@ -187,6 +187,7 @@ def start(target, users, passwds, ports, timeout_sec, thread_number, num, total,
         if test(target, retries, timeout_sec, user_agent, extra_requirements["pma_scan_http_method"][0],
                 socks_proxy, verbose_level, trying, total_req, total, num, language) is 0:
             for idir in extra_requirements["pma_scan_list"]:
+                keyboard_interrupt_flag = 0
                 if random_agent_flag:
                     user_agent = {'User-agent': random.choice(user_agent_list)}
                 t = threading.Thread(target=check,
@@ -207,7 +208,12 @@ def start(target, users, passwds, ports, timeout_sec, thread_number, num, total,
                         else:
                             break
                     except KeyboardInterrupt:
+                        keyboard_interrupt_flag = 1
                         break
+                if keyboard_interrupt_flag == 0:
+                    break
+                else:
+                    continue
         else:
             warn(messages(language, 109).format(target))
 
