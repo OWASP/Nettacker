83eb4024981b9dafb387db4d8a6f3e8c2f7d8ce6
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-03-27 19:18:53 +0430

    add first shellcode system("command");

---
+++ lib/payload/shellcode/generator/linux_x86/system/engine.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import inspect
+from lib.payload.shellcode.stack import engine as stack
+
+
+def join_payload(command):
+    return open(os.path.dirname(inspect.getfile(start)) + "/system.asm").read().format(str(command))
+
+
+def start(data):
+    command = data[0]
+    command = command.replace('[space]', ' ')
+    if int(len(command)) < 5:
+        command = str(
+            command) + '[space]&&[space]echo[space]1[space]>[space]/dev/null'  # bypass a bug in here, fix later
+    # bug in line 12 & 13, check later
+    return join_payload(stack.generate(
+        command.replace('[space]', ' '), '%ecx', 'string'))
