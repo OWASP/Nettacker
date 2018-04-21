6e3439be042008ddb7b9ab74463c383c99b54209
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-04-21 19:27:30 +0430

    add Hebrew

---
+++ lib/language/messages_iw.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def all_messages():
+    """
+    keep all messages in iw
+
+    Returns:
+        all messages in JSON
+    """
+    return \
+        {
+            "scan_started": "מנוע Nettacker התחיל ...\n\n"
+        }
