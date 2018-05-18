65b54db3d3d754725cfcd9df354feec15b1bda72
Pradeep Jairamani <pradeepjairamani22@gmail.com>
2018-05-18 23:44:20 +0530

    Resolved issue #141 and #139 (#144)
    * Resolved issue for Insecure Requests Warning message
    
    * Resolved ipaddr not found issue

--- core/attack.py
+++ core/attack.py
@@ -23,6 +23,10 @@ from lib.icmp.engine import do_one as do_one_ping
 from lib.socks_resolver.engine import getaddrinfo
 from core.alert import warn
 
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def start_attack(target, num, total, scan_method, users, passwds, timeout_sec, thread_number, ports, log_in_file,
                  time_sleep, language, verbose_level, socks_proxy, retries, ping_flag, methods_args, scan_id, scan_cmd):
