73b4d6d481acbc8b9e856957c1675ae1678afbe9
pradeepjairamani <pradeepjairamani22@gmail.com>
2018-05-22 04:37:56 -0400

    Second Commit Service Scanner

--- lib/payload/scanner/service/engine.py
+++ lib/payload/scanner/service/engine.py
@@ -7,12 +7,14 @@ from core.targets import target_type
 import requests
 import socket
 
-import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+        # Set default timeout
 
 def discover(host, port):
+#    if port==None
+
     if port==80 or port==443:
         if port == 80:
             host = "http://" + host
@@ -20,13 +22,14 @@ def discover(host, port):
             host = "https://" + host
         r = requests.get(host, verify = False)
         try:
-            return "Server: " + r.headers['server'] + " Powered by" + r.headers['X-Powered-By']
-        except:
+            return "Server: " + r.headers['server'] + " Powered by: " + r.headers['X-Powered-By']
+        except Exception:
             try:
                 return r.headers['server']
-            except:
+            except Exception:
                 return None
     else:
+        socket.setdefaulttimeout(2)
         if target_type(host) == "SINGLE_IPv6":
             s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
         else:
@@ -35,7 +38,17 @@ def discover(host, port):
             s.connect((host, port, 0, 0))
         else:
             s.connect((host, port))
-            data = s.recv(100)
-            #data = data.split()
-            return (data)
+        data = s.recv(100)
+        print (data)
+        if "ssh" in data or "SSH" in data:
+            data = data.split()
+            if data[1]!="" or data[1]!=None:
+                return "Version: " + data[0] + " Operating System: " + data[1]
+            elif data[0]!="" or data[0]!=None:
+                return "Version: " + data[0]
+            else:
+                return None
+        elif "ftp" in data or "FTP" in data:
+            data = data.split()
+            return data
         s.close()
