5ceba24fdc121c1158f0a2109d8146c1012217fb
Shaddy Garg <shaddygarg1@gmail.com>
2018-06-15 23:17:42 +0530

    Error fix due to NoneType

--- lib/transactions/maltego/nettacker_transforms/src/nettacker_transforms/transforms/admin_scan.py
+++ lib/transactions/maltego/nettacker_transforms/src/nettacker_transforms/transforms/admin_scan.py
@@ -31,7 +31,7 @@ class AdminScan(Transform):
         scan_request = request.entity
         scan_id = "".join(random.choice("0123456789abcdef") for x in range(32))
         scan_request.ports = scan_request.ports.split(', ') if scan_request.ports is not None else None
-        start(scan_request.host, [], [], ports, scan_request.timeout_sec, scan_request.thread_no,
+        start(scan_request.host, [], [], scan_request.ports, scan_request.timeout_sec, scan_request.thread_no,
               1, 1, 'abcd', 0, "en", scan_request.verbose, scan_request.socks_proxy, scan_request.retries, [], scan_id,
               "Through Maltego")
         results = find_log(scan_id, "en")
