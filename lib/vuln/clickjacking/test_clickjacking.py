ac079baf979b675fcf1f8e4cd426a19186bf531e
aman566 <aman.g17@iiits.in>
2020-05-10 13:23:39 +0530

    Improved test using Parametrize decorators to avoid DRY principle

--- lib/vuln/clickjacking/test_clickjacking.py
+++ lib/vuln/clickjacking/test_clickjacking.py
@@ -2,11 +2,17 @@ import pytest
 from . import engine
 
 
-def test_clickjacking():
+@pytest.mark.parametrize('target, port, timeout_sec, log_in_file, language, time_sleep, thread_tmp_filename, socks_proxy, scan_id, scan_cmd, result',
+                        [
+                            ("https://google.com", 443, 3.0, "./test.html", "en", 0.0, "./thread", None, "nettacker.py -m clickjacking_vuln -i https://google.com", "7a088343646d486eddefa4028013a6b", False),
+                            ("https://github.com", 443, 3.0, "./test.html", "en", 0.0, "./thread", None, "nettacker.py -m clickjacking_vuln -i http://github.com", "6eaa4fd99a3a881b9a426be1014f6442", False),
+                             ("http://52.198.49.156/", 443, 3.0, "./test.html", "en", 0.0, "./thread", None, "nettacker.py -m clickjacking_vuln -i http://52.198.49.156/", "6eaa4fd99a3a881b9a426be1014f6442", True)
+                        ])
+def test_clickjacking(target, port, timeout_sec, log_in_file, language, time_sleep, thread_tmp_filename, socks_proxy, scan_id, scan_cmd, result):
 
-    assert engine.clickjacking("https://google.com", 443, 3.0, "./test.html", "en", 0.0, "./thread", None, "nettacker.py -m clickjacking_vuln -i https://analysed.in", "7a088343646d486eddefa4028013a6b") == False
+    if(not result):
+        assert engine.clickjacking(target, port, timeout_sec, log_in_file, language, time_sleep, thread_tmp_filename, socks_proxy, scan_id, scan_cmd) == result
+    else:
+        with pytest.raises(Exception):
+            assert engine.clickjacking(target, port, timeout_sec, log_in_file, language, time_sleep, thread_tmp_filename, socks_proxy, scan_id, scan_cmd) == result
 
-    assert engine.clickjacking("https://github.com", 443, 3.0, "./test.html", "en", 0.0, "./thread", None, "nettacker.py -m clickjacking_vuln -i http://3.12.66.171/hub/login", "6eaa4fd99a3a881b9a426be1014f6442") == False
-    
-    with pytest.raises(Exception):
-        assert engine.clickjacking("http://52.198.49.156/", 443, 3.0, "./test.html", "en", 0.0, "./thread", None, "nettacker.py -m clickjacking_vuln -i http://52.198.49.156/", "6eaa4fd99a3a881b9a426be1014f6442") == True
