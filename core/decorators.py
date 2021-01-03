64c7d699ad4a34bbb412eaecae608c9c4271059d
itsdivyanshjain <divyanshjain3110@gmail.com>
2021-01-03 10:46:28 +0530

    flake8 and pylint tested

--- core/decorators.py
+++ core/decorators.py
@@ -1,11 +1,11 @@
-from core.alert import *
+from core.alert import warn, messages
 from core.targets import target_type
 from core.targets import target_to_host
 
 
 def requirements_check(**kwargs):
     '''
-    Args: 
+    Args:
         target_to_host = default(true), scan_name
 
     E.g:
@@ -31,9 +31,16 @@ def requirements_check(**kwargs):
         takes start function in general
         '''
 
-        def requirement_checker_with_func_args(target, users, passwds, ports, timeout_sec, thread_number, num, total, log_in_file, time_sleep, language,
-                                               verbose_level, socks_proxy, retries, methods_args, scan_id, scan_cmd):
-            if target_type(target) != 'SINGLE_IPv4' or target_type(target) != 'DOMAIN' or target_type(target) != 'HTTP':
+        def requirement_checker(target, users, passwds, ports, timeout_sec,
+                                thread_number, num, total, log_in_file,
+                                time_sleep, language, verbose_level,
+                                socks_proxy, retries, methods_args,
+                                scan_id, scan_cmd):
+
+            if (target_type(target) != 'SINGLE_IPv4' or
+                    target_type(target) != 'DOMAIN' or
+                    target_type(target) != 'HTTP'):
+
                 if target_to_host_bool and target_type(target) == 'HTTP':
                     target = target_to_host(target)
 
@@ -47,18 +54,22 @@ def requirements_check(**kwargs):
                     for extra_requirement in extra_requirements_dict():
                         if extra_requirement in methods_args:
                             new_extra_requirements[
-                                extra_requirement] = methods_args[extra_requirement]
+                                extra_requirement] = methods_args[
+                                    extra_requirement]
                 extra_requirements = new_extra_requirements
                 func_name.__globals__[
                     'extra_requirements'] = extra_requirements
                 # info(messages(language, "done"))
-                return func_name(target, users, passwds, ports, timeout_sec, thread_number, num, total, log_in_file, time_sleep, language,
-                                 verbose_level, socks_proxy, retries, methods_args, scan_id, scan_cmd)
+                return func_name(target, users, passwds, ports, timeout_sec,
+                                 thread_number, num, total, log_in_file,
+                                 time_sleep, language, verbose_level,
+                                 socks_proxy, retries, methods_args,
+                                 scan_id, scan_cmd)
 
             else:
                 warn(messages(language, "input_target_error").format(
                     scan_name, target))
 
-        return requirement_checker_with_func_args
+        return requirement_checker
 
     return start_function_taker
