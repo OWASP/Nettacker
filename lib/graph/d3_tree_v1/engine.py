8a56c3d83c68558775a547c734cec6427875a51a
aman566 <aman.g17@iiits.in>
2021-01-11 20:51:09 +0530

    flake8 errors fixed

--- lib/graph/d3_tree_v1/engine.py
+++ lib/graph/d3_tree_v1/engine.py
@@ -11899,6 +11899,6 @@ treeJSON = d3.json("https://github.com/zdresearch/OWASP-Nettacker", function(err
         .replace('__title_to_replace__', messages(language, "pentest_graphs")) \
         .replace('__description_to_replace__', messages(language, "graph_message")) \
         .replace('__html_title_to_replace__', messages(language, "nettacker_report"))
-    if version() is 2:
+    if version() == 2:
         return data.decode('utf8')
     return data
