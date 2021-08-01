d0c62679ff40d560bc3366753fc4272adccdb0ec
Ali Razmjoo <ali.razmjoo@owasp.org>
2021-08-01 23:08:00 +0200

    new enhancement and PoC subdomain scan

--- core/module_protocols/http.py
+++ core/module_protocols/http.py
@@ -12,61 +12,77 @@ from core.alert import (info,
 def reverse_and_regex_condition(regex, reverse):
     if regex:
         if reverse:
-            return False
-        return True
+            return []
+        return regex
     else:
         if reverse:
-            return True
-        return False
+            return regex
+        return []
 
 
 def response_conditions_matched(sub_step, response):
     condition_type = sub_step['response']['condition_type']
     conditions = sub_step['response']['conditions']
-    condition_results = []
+    condition_results = {}
     if 'reason' in conditions:
-        regex = re.search(str(conditions['reason']['regex']).encode(), response.reason.encode())
+        regex = re.findall(re.compile(conditions['reason']['regex']), response.reason)
         reverse = conditions['reason']['reverse']
-        condition_results.append(reverse_and_regex_condition(regex, reverse))
+        condition_results['reason'] = reverse_and_regex_condition(regex, reverse)
     if 'status_code' in conditions:
-        regex = re.search(str(conditions['status_code']['regex']).encode(), str(response.status_code).encode())
+        regex = re.findall(re.compile(conditions['status_code']['regex']), str(response.status_code))
         reverse = conditions['status_code']['reverse']
-        condition_results.append(reverse_and_regex_condition(regex, reverse))
+        condition_results['status_code'] = reverse_and_regex_condition(regex, reverse)
     if 'content' in conditions:
-        regex = re.search(str(conditions['content']['regex']).encode(), str(response.content).encode())
+        regex = re.findall(re.compile(conditions['content']['regex']), response.content.decode())
         reverse = conditions['content']['reverse']
-        condition_results.append(reverse_and_regex_condition(regex, reverse))
+        condition_results['content'] = reverse_and_regex_condition(regex, reverse)
     if 'headers' in conditions:
         for header in conditions['headers']:
             if header in response.headers:
-                regex = re.search(str(conditions['headers'][header]['regex']).encode(),
-                                  str(response.headers[header]).encode())
+                regex = re.findall(
+                    re.compile(conditions['headers'][header]['regex']),
+                    response.headers[header]
+                )
                 reverse = conditions['headers'][header]['reverse']
-                condition_results.append(reverse_and_regex_condition(regex, reverse))
+                condition_results['headers'] = reverse_and_regex_condition(regex, reverse)
     if 'responsetime' in conditions:
         if conditions['responsetime'].startswith(">="):
-            condition_results.append(response.elapsed.total_seconds() >= float(conditions['responsetime'].split()[-1]))
+            condition_results['responsetime'] = response.elapsed.total_seconds() if (
+                    response.elapsed.total_seconds() >= float(conditions['responsetime'].split()[-1])
+            ) else False
         if conditions['responsetime'].startswith("=="):
-            condition_results.append(response.elapsed.total_seconds() == float(conditions['responsetime'].split()[-1]))
+            condition_results['responsetime'] = response.elapsed.total_seconds() if (
+                    response.elapsed.total_seconds() == float(conditions['responsetime'].split()[-1])
+            ) else False
         if conditions['responsetime'].startswith("<="):
-            condition_results.append(response.elapsed.total_seconds() <= float(conditions['responsetime'].split()[-1]))
+            condition_results['responsetime'] = response.elapsed.total_seconds() if (
+                    response.elapsed.total_seconds() <= float(conditions['responsetime'].split()[-1])
+            ) else False
         if conditions['responsetime'].startswith("!="):
-            condition_results.append(response.elapsed.total_seconds() != float(conditions['responsetime'].split()[-1]))
+            condition_results['responsetime'] = response.elapsed.total_seconds() if (
+                    response.elapsed.total_seconds() != float(conditions['responsetime'].split()[-1])
+            ) else False
         if conditions['responsetime'].startswith("<"):
-            condition_results.append(response.elapsed.total_seconds() < float(conditions['responsetime'].split()[-1]))
+            condition_results['responsetime'] = response.elapsed.total_seconds() if (
+                    response.elapsed.total_seconds() < float(conditions['responsetime'].split()[-1])
+            ) else False
         if conditions['responsetime'].startswith(">"):
-            condition_results.append(response.elapsed.total_seconds() <= float(conditions['responsetime'].split()[-1]))
+            condition_results['responsetime'] = response.elapsed.total_seconds() if (
+                    response.elapsed.total_seconds() > float(conditions['responsetime'].split()[-1])
+            ) else False
     if condition_type.lower() == "or":
-        if True in condition_results:
-            return True
+        # if one of the values are matched, it will be a string or float object in the array
+        # we count False in the array and if it's not all []; then we know one of the conditions is matched.
+        if list(condition_results.values()).count([]) != len(list(condition_results.values())):
+            return condition_results
         else:
-            return False
+            return []
     if condition_type.lower() == "and":
-        if False in condition_results:
-            return False
+        if [] in condition_results.values():
+            return []
         else:
-            return True
-    return False
+            return condition_results
+    return []
 
 
 class engine:
@@ -84,9 +100,14 @@ class engine:
             return False
         sub_step['method'] = backup_method
         sub_step['response'] = backup_response
-        if response_conditions_matched(sub_step, response):
-            info(json.dumps(sub_step) + ' ' + json.dumps(vars(response.headers)) + ' ' + str(response.status_code))
+        conditions_results = response_conditions_matched(sub_step, response)
+        if conditions_results:
+            info(
+                json.dumps(sub_step) + ' ' + json.dumps(conditions_results)
+            )
             return True
         else:
-            verbose_info(json.dumps(sub_step) + ' ' + json.dumps(vars(response.headers)) + ' ' + str(response.status_code))
+            verbose_info(
+                json.dumps(sub_step) + ' ' + str(conditions_results)
+            )
             return False
