e15bf83bb6406da4704e5dc7a591d0687a1ddea9
Ali Razmjoo <ali.razmjoo@owasp.org>
2021-08-04 15:41:11 +0200

    service scan PoC

--- core/module_protocols/socket.py
+++ core/module_protocols/socket.py
@@ -8,10 +8,48 @@ from core.utility import reverse_and_regex_condition
 from core.utility import process_conditions
 
 
+def receive_all(socket_connection, limit=4196):
+    """
+    receive all data from a socket
+    Args:
+        socket_connection: python socket
+        limit: limit size to get response
+    Returns:
+        response or b""
+    """
+    response_content = ""
+    while len(response_content) < limit:
+        try:
+            response_byte = socket_connection.recv(1)
+            if response_byte != b"":
+                response_content += response_byte.decode()
+            else:
+                break
+        except Exception:
+            break
+    return response_content
+
+
 def response_conditions_matched(sub_step, response):
+    conditions = sub_step['response']['conditions']
+    condition_type = sub_step['response']['condition_type']
+    condition_results = {}
     if sub_step['method'] == 'tcp_connect_only':
         if response:
             return response
+    if sub_step['method'] == 'tcp_connect_send_and_receive':
+        if response:
+            received_content = response['response']
+            for condition in conditions:
+                regex = re.findall(re.compile(conditions[condition]['regex']), received_content)
+                reverse = conditions[condition]['reverse']
+                condition_results[condition] = reverse_and_regex_condition(regex, reverse)
+            for condition in copy.deepcopy(condition_results):
+                if not condition_results[condition]:
+                    del condition_results[condition]
+            if 'open_port' in condition_results and len(condition_results) > 1:
+                del condition_results['open_port']
+            return condition_results
     return []
 
 
@@ -20,10 +58,25 @@ class NettackerSocket:
         socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_connection.settimeout(timeout)
         socket_connection.connect((host, int(port)))
+        peer_name = socket_connection.getpeername()
         socket_connection.close()
         return {
-            "peer_name": socket_connection.getpeername(),
+            "peer_name": peer_name,
             "service": socket.getservbyport(int(port))
+        }
+
+    def tcp_connect_send_and_receive(host, port, timeout):
+        socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_connection.settimeout(timeout)
+        socket_connection.connect((host, int(port)))
+        peer_name = socket_connection.getpeername()
+        socket_connection.send(b"ABC\x00\r\n" * 10)
+        response = receive_all(socket_connection)
+        socket_connection.close()
+        return {
+            "peer_name": peer_name,
+            "service": socket.getservbyport(int(port)),
+            "response": response
         }
 
 
