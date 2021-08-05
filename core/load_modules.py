c6f4a56415be79b263333072fa5980c834f559b7
Ali Razmjoo <ali.razmjoo@owasp.org>
2021-08-05 16:25:36 +0200

    implement socks_proxy on yaml modules

--- core/load_modules.py
+++ core/load_modules.py
@@ -4,6 +4,46 @@
 import os
 from glob import glob
 from io import StringIO
+import socks
+import socket
+
+
+def getaddrinfo(*args):
+    """
+    same getaddrinfo() used in socket except its resolve addresses with socks proxy
+
+    Args:
+        args: *args
+
+    Returns:
+        getaddrinfo
+    """
+    return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+
+
+def set_socks_proxy(socks_proxy):
+    if socks_proxy:
+        socks_version = socks.SOCKS5 if socks_proxy.startswith('socks5://') else socks.SOCKS4
+        socks_proxy = socks_proxy.split('://')[1] if '://' in socks_proxy else socks_proxy
+        if '@' in socks_proxy:
+            socks_username = socks_proxy.split(':')[0]
+            socks_password = socks_proxy.split(':')[1].split('@')[0]
+            socks.set_default_proxy(
+                socks_version,
+                str(socks_proxy.rsplit('@')[1].rsplit(':')[0]),  # hostname
+                int(socks_proxy.rsplit(':')[-1]),  # port
+                username=socks_username,
+                password=socks_password
+            )
+        else:
+            socks.set_default_proxy(
+                socks_version,
+                str(socks_proxy.rsplit(':')[0]),  # hostname
+                int(socks_proxy.rsplit(':')[1])  # port
+            )
+        return socks.socksocket, getaddrinfo
+    else:
+        return socket.socket, socket.getaddrinfo
 
 
 class NettackerModules:
@@ -140,6 +180,7 @@ def perform_scan(options, target, module_name, scan_unique_id):
     from core.alert import (info,
                             messages)
 
+    socket.socket, socket.getaddrinfo = set_socks_proxy(options.socks_proxy)
     options.target = target
     validate_module = NettackerModules()
     validate_module.module_name = module_name
--- lib/socks_resolver/readme.md
+++
@@ -1,4 +0,0 @@
-OWASP Nettacker Socks resolver Library
-======================================
-
-OWASP Nettacker socks resolver lib stored in here.
