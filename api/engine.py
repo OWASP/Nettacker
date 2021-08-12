b7abbf591053cbd37a226ccd650cd636563d814c
itsdivyanshjain <divyanshjain3110@gmail.com>
2021-08-13 00:53:28 +0530

    filename fix

--- api/engine.py
+++ api/engine.py
@@ -370,7 +370,7 @@ def get_results_json():
     scan_details = session.query(Report).filter(Report.id == result_id).first()
     data = __logs_by_scan_id(scan_details.scan_unique_id)
     json_object = json.dumps(data)
-    filename = ".".join(scan_details.report_path_filename.split('.')[:-1]) + '.json'
+    filename = ".".join(scan_details.report_path_filename.split('.')[:-1])[1:] + '.json'
     return Response(
         json_object,
         mimetype='application/json',
@@ -401,7 +401,7 @@ def get_results_csv():  # todo: need to fix time format
     scan_details = session.query(Report).filter(Report.id == result_id).first()
     data = __logs_by_scan_id(scan_details.scan_unique_id)
     keys = data[0].keys()
-    filename = ".".join(scan_details.report_path_filename.split('.')[:-1]) + '.csv'
+    filename = ".".join(scan_details.report_path_filename.split('.')[:-1])[1:] + '.csv'
     with open(filename, "w") as report_path_filename:
         dict_writer = csv.DictWriter(
             report_path_filename,
