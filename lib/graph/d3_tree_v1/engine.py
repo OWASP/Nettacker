74638ba932510e8d9f117be93f1e2accce7eec77
Ali Razmjoo <ali.razmjoo@owasp.org>
2021-08-25 19:29:58 +0200

    more refactor

--- lib/graph/d3_tree_v1/engine.py
+++ lib/graph/d3_tree_v1/engine.py
@@ -23,26 +23,54 @@ def start(events):
         "children": {}
     }
     # get data for normalised_json
-    for each_scan in events:
-        if each_scan['target'] not in normalisedjson['children']:
-            normalisedjson['children'].update({each_scan['target']: {}})
-            normalisedjson['children'][each_scan['target']].update(
-                {each_scan['module_name']: []})
+    for event in events:
+        if event['target'] not in normalisedjson['children']:
+            normalisedjson['children'].update(
+                {
+                    event['target']: {}
+                }
+            )
+            normalisedjson['children'][event['target']].update(
+                {
+                    event['module_name']: []
+                }
+            )
 
-        if each_scan['module_name'] not in normalisedjson['children'][each_scan['target']]:
-            normalisedjson['children'][each_scan['target']].update(
-                {each_scan['module_name']: []})
-        normalisedjson['children'][each_scan['target']][each_scan['module_name']].append(
-            f"target: {each_scan['target']}, module_name: {each_scan['module_name']}, options: {each_scan['options']}, event: {each_scan['event']}")
+        if event['module_name'] not in normalisedjson['children'][event['target']]:
+            normalisedjson['children'][event['target']].update(
+                {
+                    event['module_name']: []
+                }
+            )
+        normalisedjson['children'][event['target']][event['module_name']].append(
+            f"target: {event['target']}, module_name: {event['module_name']}, options: "
+            f"{event['options']}, event: {event['event']}"
+        )
     # define a d3_structure_json
-    d3_structure = {"name": "Starting attack",
-                    "children": []}
+    d3_structure = {
+        "name": "Starting attack",
+                    "children": []
+    }
     # get data for normalised_json
     for target in list(normalisedjson['children'].keys()):
-        for otype in list(normalisedjson['children'][target].keys()):
-            for description in normalisedjson["children"][target][otype]:
-                children_array = [{"name": otype, "children": [{"name": description}]}]
-                d3_structure["children"].append({"name": target, "children": children_array})
+        for module_name in list(normalisedjson['children'][target].keys()):
+            for description in normalisedjson["children"][target][module_name]:
+                children_array = [
+                    {
+                        "name": module_name,
+                        "children": [
+                            {
+                                "name": description
+                            }
+                        ]
+                    }
+                ]
+                d3_structure["children"].append(
+                    {
+                        "name": target,
+                        "children": children_array
+                    }
+                )
 
     from config import nettacker_paths
     data = open(
@@ -50,8 +78,17 @@ def start(events):
             nettacker_paths()['web_static_files_path'],
             'report/d3_tree_v1.html'
         )
-    ).read().replace('__data_will_locate_here__', json.dumps(d3_structure)) \
-        .replace('__title_to_replace__', messages("pentest_graphs")) \
-        .replace('__description_to_replace__', messages("graph_message")) \
-        .replace('__html_title_to_replace__', messages("nettacker_report"))
+    ).read().replace(
+        '__data_will_locate_here__',
+        json.dumps(d3_structure)
+    ).replace(
+        '__title_to_replace__',
+        messages("pentest_graphs")
+    ).replace(
+        '__description_to_replace__',
+        messages("graph_message")
+    ).replace(
+        '__html_title_to_replace__',
+        messages("nettacker_report")
+    )
     return data
