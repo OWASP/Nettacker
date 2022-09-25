121e6f1eb1e41eaf287805998c61852d9b30b590
itsdivyanshjain <divyanshjain3110@gmail.com>
2022-09-25 10:08:12 -0400

    added copy to clipboard json_event

--- web/static/report/json_parse.js
+++ web/static/report/json_parse.js
@@ -3,9 +3,13 @@
     document.getElementById("json_length").innerText = "";
     renderjson.set_icons('▶','▼')
     renderjson.set_collapse_msg(function (asd) {return "...";});
+    arr = [];
     for (let i=1; i<=length; i++) {
         value = document.getElementById("json_event_"+i).innerText;
+        arr.push(value);
         document.getElementById("json_event_"+i).innerText = "";
+        document.getElementById("json_clipboard_"+i).addEventListener("click", function() { navigator.clipboard.writeText(arr[i-1]); });
+        //document.getElementById("json_clipboard"+i).json_clipboard_
         document.getElementById("json_event_"+i).appendChild(renderjson(JSON.parse(value)));
     }
 </script>
