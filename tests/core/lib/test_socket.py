825bc14aadd899c2b4473d5b9a1491469f95be9b
Achintya Jai <153343775+pUrGe12@users.noreply.github.com>
2025-04-28 03:48:11 +0530

    Implementing logging response_dependent conditions in socket.py (#1060)
    * logging matched services along with output from tcp_connect_send_and_recieve
    
    * fixed tests
    
    * fixed module.py

--- tests/core/lib/test_socket.py
+++ tests/core/lib/test_socket.py
@@ -9,6 +9,7 @@ class Responses:
 
     tcp_connect_send_and_receive = {
         "response": 'HTTP/1.1 400 Bad Request\r\nServer: Apache/2.4.62 (Debian)\r\nContent-Length: 302\r\nConnection: close\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">\n<html><head>\n<title>400 Bad Request</title>\n</head><body>\n<h1>Bad Request</h1>\n<p>Your browser sent a request that this server could not understand.<br />\n</p>\n<hr>\n<address>Apache/2.4.62 (Debian)</address>\n</body></html>\n',
+        "service": "http",
         "peer_name": (
             "127.0.0.1",
             80,
@@ -31,69 +32,75 @@ class Substeps:
         "method": "tcp_connect_send_and_receive",
         "response": {
             "condition_type": "or",
+            "log": "response_dependent['service']",
             "conditions": {
-                "open_port": {"regex": "", "reverse": False},
-                "ftp": {
-                    "regex": "220-You are user number|530 USER and PASS required|Invalid command: try being more creative|220 \\S+ FTP (Service|service|Server|server)|220 FTP Server ready|Directory status|Service closing control connection|Requested file action|Connection closed; transfer aborted|Directory not empty",
-                    "reverse": False,
-                },
-                "ftps": {
-                    "regex": "220-You are user number|530 USER and PASS required|Invalid command: try being more creative|220 \\S+ FTP (Service|service|Server|server)|220 FTP Server ready|Directory status|Service closing control connection|Requested file action|Connection closed; transfer aborted|Directory not empty",
-                    "reverse": False,
-                },
-                "http": {
-                    "regex": "HTTPStatus.BAD_REQUEST|HTTP\\/[\\d.]+\\s+[\\d]+|Server: |Content-Length: \\d+|Content-Type: |Access-Control-Request-Headers: |Forwarded: |Proxy-Authorization: |User-Agent: |X-Forwarded-Host: |Content-MD5: |Access-Control-Request-Method: |Accept-Language: ",
-                    "reverse": False,
-                },
-                "imap": {
-                    "regex": "Internet Mail Server|IMAP4 service|BYE Hi This is the IMAP SSL Redirect|LITERAL\\+ SASL\\-IR LOGIN\\-REFERRALS ID ENABLE IDLE AUTH\\=PLAIN AUTH\\=LOGIN AUTH\\=DIGEST\\-MD5 AUTH\\=CRAM-MD5|CAPABILITY completed|OK IMAPrev1|LITERAL\\+ SASL\\-IR LOGIN\\-REFERRALS ID ENABLE IDLE NAMESPACE AUTH\\=PLAIN AUTH\\=LOGIN|BAD Error in IMAP command received by server|IMAP4rev1 SASL-IR|OK \\[CAPABILITY IMAP4rev1",
-                    "reverse": False,
-                },
-                "mariadb": {
-                    "regex": "is not allowed to connect to this MariaDB server",
-                    "reverse": False,
-                },
-                "mysql": {
-                    "regex": "is not allowed to connect to this MySQL server",
-                    "reverse": False,
-                },
-                "nntp": {
-                    "regex": "NetWare\\-News\\-Server|NetWare nntpd|nntp|Leafnode nntpd|InterNetNews NNRP server INN",
-                    "reverse": False,
-                },
-                "pop3": {
-                    "regex": "POP3|POP3 gateway ready|POP3 Server|Welcome to mpopd|OK Hello there",
-                    "reverse": False,
-                },
-                "pop3s": {
-                    "regex": "POP3|POP3 gateway ready|POP3 Server|Welcome to mpopd|OK Hello there",
-                    "reverse": False,
-                },
-                "portmap": {
-                    "regex": "Program\tVersion\tProtocol\tPort|portmapper|nfs\t2|nlockmgr\t1",
-                    "reverse": False,
-                },
-                "postgressql": {
-                    "regex": "FATAL 1\\:  invalid length of startup packet|received invalid response to SSL negotiation\\:|unsupported frontend protocol|fe\\_sendauth\\: no password supplied|no pg\\_hba\\.conf entry for host",
-                    "reverse": False,
-                },
-                "pptp": {"regex": "Hostname: pptp server|Vendor: Fortinet pptp", "reverse": False},
-                "smtp": {
-                    "regex": "Fidelix Fx2020|ESMTP|Server ready|SMTP synchronization error|220-Greetings|ESMTP Arnet Email Security|SMTP 2.0",
-                    "reverse": False,
-                },
-                "smtps": {
-                    "regex": "Fidelix Fx2020|ESMTP|Server ready|SMTP synchronization error|220-Greetings|ESMTP Arnet Email Security|SMTP 2.0",
-                    "reverse": False,
-                },
-                "rsync": {"regex": "@RSYNCD\\:", "reverse": False},
-                "ssh": {
-                    "regex": "openssh|\\-OpenSSH\\_|\\r\\nProtocol mism|\\_sshlib|\\x00\\x1aversion info line too long|SSH Windows NT Server|WinNT sshd|sshd| SSH Secure Shell|WinSSHD",
-                    "reverse": False,
-                },
-                "telnet": {
-                    "regex": "Check Point FireWall-1 authenticated Telnet server running on|Raptor Firewall Secure Gateway|No more connections are allowed to telnet server|Closing Telnet connection due to host problems|NetportExpress|WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING|Login authentication|recommended to use Stelnet|is not a secure protocol|Welcome to Microsoft Telnet Servic|no decompiling or reverse-engineering shall be allowed",
-                    "reverse": False,
+                "service": {
+                    "open_port": {"regex": "", "reverse": False},
+                    "ftp": {
+                        "regex": "220-You are user number|530 USER and PASS required|Invalid command: try being more creative|220 \\S+ FTP (Service|service|Server|server)|220 FTP Server ready|Directory status|Service closing control connection|Requested file action|Connection closed; transfer aborted|Directory not empty",
+                        "reverse": False,
+                    },
+                    "ftps": {
+                        "regex": "220-You are user number|530 USER and PASS required|Invalid command: try being more creative|220 \\S+ FTP (Service|service|Server|server)|220 FTP Server ready|Directory status|Service closing control connection|Requested file action|Connection closed; transfer aborted|Directory not empty",
+                        "reverse": False,
+                    },
+                    "http": {
+                        "regex": "HTTPStatus.BAD_REQUEST|HTTP\\/[\\d.]+\\s+[\\d]+|Server: |Content-Length: \\d+|Content-Type: |Access-Control-Request-Headers: |Forwarded: |Proxy-Authorization: |User-Agent: |X-Forwarded-Host: |Content-MD5: |Access-Control-Request-Method: |Accept-Language: ",
+                        "reverse": False,
+                    },
+                    "imap": {
+                        "regex": "Internet Mail Server|IMAP4 service|BYE Hi This is the IMAP SSL Redirect|LITERAL\\+ SASL\\-IR LOGIN\\-REFERRALS ID ENABLE IDLE AUTH\\=PLAIN AUTH\\=LOGIN AUTH\\=DIGEST\\-MD5 AUTH\\=CRAM-MD5|CAPABILITY completed|OK IMAPrev1|LITERAL\\+ SASL\\-IR LOGIN\\-REFERRALS ID ENABLE IDLE NAMESPACE AUTH\\=PLAIN AUTH\\=LOGIN|BAD Error in IMAP command received by server|IMAP4rev1 SASL-IR|OK \\[CAPABILITY IMAP4rev1",
+                        "reverse": False,
+                    },
+                    "mariadb": {
+                        "regex": "is not allowed to connect to this MariaDB server",
+                        "reverse": False,
+                    },
+                    "mysql": {
+                        "regex": "is not allowed to connect to this MySQL server",
+                        "reverse": False,
+                    },
+                    "nntp": {
+                        "regex": "NetWare\\-News\\-Server|NetWare nntpd|nntp|Leafnode nntpd|InterNetNews NNRP server INN",
+                        "reverse": False,
+                    },
+                    "pop3": {
+                        "regex": "POP3|POP3 gateway ready|POP3 Server|Welcome to mpopd|OK Hello there",
+                        "reverse": False,
+                    },
+                    "pop3s": {
+                        "regex": "POP3|POP3 gateway ready|POP3 Server|Welcome to mpopd|OK Hello there",
+                        "reverse": False,
+                    },
+                    "portmap": {
+                        "regex": "Program\tVersion\tProtocol\tPort|portmapper|nfs\t2|nlockmgr\t1",
+                        "reverse": False,
+                    },
+                    "postgressql": {
+                        "regex": "FATAL 1\\:  invalid length of startup packet|received invalid response to SSL negotiation\\:|unsupported frontend protocol|fe\\_sendauth\\: no password supplied|no pg\\_hba\\.conf entry for host",
+                        "reverse": False,
+                    },
+                    "pptp": {
+                        "regex": "Hostname: pptp server|Vendor: Fortinet pptp",
+                        "reverse": False,
+                    },
+                    "smtp": {
+                        "regex": "Fidelix Fx2020|ESMTP|Server ready|SMTP synchronization error|220-Greetings|ESMTP Arnet Email Security|SMTP 2.0",
+                        "reverse": False,
+                    },
+                    "smtps": {
+                        "regex": "Fidelix Fx2020|ESMTP|Server ready|SMTP synchronization error|220-Greetings|ESMTP Arnet Email Security|SMTP 2.0",
+                        "reverse": False,
+                    },
+                    "rsync": {"regex": "@RSYNCD\\:", "reverse": False},
+                    "ssh": {
+                        "regex": "openssh|\\-OpenSSH\\_|\\r\\nProtocol mism|\\_sshlib|\\x00\\x1aversion info line too long|SSH Windows NT Server|WinNT sshd|sshd| SSH Secure Shell|WinSSHD",
+                        "reverse": False,
+                    },
+                    "telnet": {
+                        "regex": "Check Point FireWall-1 authenticated Telnet server running on|Raptor Firewall Secure Gateway|No more connections are allowed to telnet server|Closing Telnet connection due to host problems|NetportExpress|WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING|Login authentication|recommended to use Stelnet|is not a secure protocol|Welcome to Microsoft Telnet Servic|no decompiling or reverse-engineering shall be allowed",
+                        "reverse": False,
+                    },
                 },
             },
         },
@@ -150,7 +157,15 @@ class TestSocketMethod(TestCase):
                 )
             ),
             sorted(
-                {"http": ["Content-Type: ", "Content-Length: 302", "HTTP/1.1 400", "Server: "]}
+                {
+                    "http": ["Content-Type: ", "Content-Length: 302", "HTTP/1.1 400", "Server: "],
+                    "log": [
+                        "{'running_service': 'http', 'matched_regex': ['Server: ', 'HTTP/1.1 400', 'Content-Length: 302', 'Content-Type: '], 'default_service': 'http', 'ssl_flag': True}"
+                    ],
+                    "service": [
+                        "{'running_service': 'http', 'matched_regex': ['Server: ', 'HTTP/1.1 400', 'Content-Length: 302', 'Content-Type: '], 'default_service': 'http', 'ssl_flag': True}"
+                    ],
+                }
             ),
         )
 
