87dbff16269f5a02d564c11b3b3083dfc0f1e5d1
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2025-06-10 18:06:44 -0700

    Update tests

--- tests/lib/payloads/test_passwords.py
+++ tests/lib/payloads/test_passwords.py
@@ -1,19 +1,15 @@
 from collections import Counter
 from pathlib import Path
 
-import pytest
-
-top_1000_common_passwords_path = Path(
-    "nettacker/lib/payloads/passwords/top_1000_common_passwords.txt"
-)
 nettacker_path = Path(__file__).parent.parent.parent.parent
 
 
-@pytest.mark.xfail(reason="It currently contains 1001 passwords.")
 def test_top_1000_common_passwords():
-    full_path = nettacker_path / top_1000_common_passwords_path
-    with open(full_path) as f:
-        top_1000_passwords = [line.strip() for line in f.readlines()]
+    top_1000_passwords_file_path = (
+        nettacker_path / "nettacker/lib/payloads/passwords/top_1000_common_passwords.txt"
+    )
+    with open(top_1000_passwords_file_path) as f:
+        top_1000_passwords = [line.strip() for line in f.readlines() if line.strip()]
 
     assert len(top_1000_passwords) == 1000, "There should be exactly 1000 passwords"
 
