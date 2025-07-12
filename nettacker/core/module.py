f8d06710253944431d5352b28e19a2b8da4dbee8
pUrGe12 <helloitsme0218@gmail.com>
2025-07-12 21:36:43 +0530

    not relying on try and except

--- nettacker/core/module.py
+++ nettacker/core/module.py
@@ -111,16 +111,12 @@ class Module:
     def generate_loops(self):
         if self.module_inputs["excluded_ports"]:
             excluded_port_set = set(self.module_inputs["excluded_ports"])
-            try:
+            if self.module_content and "ports" in self.module_content["payloads"][0]["steps"][0]:
                 all_ports = self.module_content["payloads"][0]["steps"][0]["ports"]
                 all_ports[:] = [port for port in all_ports if port not in excluded_port_set]
-                self.module_content["payloads"] = expand_module_steps(
-                    self.module_content["payloads"]
-                )
-            except Exception:
-                self.module_content["payloads"] = expand_module_steps(
-                    self.module_content["payloads"]
-                )
+            self.module_content["payloads"] = expand_module_steps(self.module_content["payloads"])
+        else:
+            self.module_content["payloads"] = expand_module_steps(self.module_content["payloads"])
 
     def sort_loops(self):
         steps = []
