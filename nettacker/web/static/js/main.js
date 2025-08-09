c77246f70095c64d1ef56f3f9656cea33f9a8f4b
Davda James <151067328+Davda-James@users.noreply.github.com>
2025-08-09 20:49:31 +0530

    Fixed the issue of select all profiles button (#1117)
    * earlier selecting the select all profiles selects the all scan methods instead of profiles, fixed that now select all profiles works perfectly
    
    * was taking all_profiles also its name, fixed that by adding condition

--- nettacker/web/static/js/main.js
+++ nettacker/web/static/js/main.js
@@ -351,8 +351,10 @@ $(document).ready(function () {
     var p = [];
     var n = 0;
     $("#profiles input:checked").each(function () {
-      p[n] = this.id;
-      n += 1;
+      if (this.id !== "all_profiles") {
+        p[n] = this.id;
+        n += 1;
+      }
     });
     var profiles = p.join(",");
 
@@ -674,6 +676,10 @@ $(document).ready(function () {
 
   $(".checkbox-vulnerability").click(function () {
     $(".checkbox-vuln-module").prop("checked", $(this).prop("checked"));
+  });
+
+  $(".check-all-profiles").click(function () {
+    $("#profiles input[type='checkbox']").not(this).prop("checked", $(this).prop("checked"));
   });
 
   $(".check-all-scans").click(function () {
