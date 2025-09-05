5cf89db4c21e7c44f5eac1869b7cb1e82f1e1eaf
pUrGe12 <helloitsme0218@gmail.com>
2025-09-05 22:39:31 +0530

    update documentation for databases

--- nettacker/config.py
+++ nettacker/config.py
@@ -84,7 +84,7 @@ class DbConfig(ConfigBase):
         DATABASE as the name of the db user wants
         Set the journal_mode (default="WAL") and
         synchronous_mode (default="NORMAL"). Rest
-        of the fields can be left emptyAdd commentMore actions
+        of the fields can be left empty
         This is the default database:
         str(CWD / ".data/nettacker.db")
     For mysql users:
