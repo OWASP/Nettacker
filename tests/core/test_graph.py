9badba7a84e07c2161678babc2ecd87050d2d575
rajanpanth <rajan.pantha@samriddhicollege.edu.np>
2026-08-15 21:11:32 +0545

    fix: stop graph import failures from continuing

--- tests/core/test_graph.py
+++ tests/core/test_graph.py
@@ -33,7 +33,6 @@ def test_build_graph_success(mock_import_module):
 
 @patch("nettacker.core.graph.die_failure")
 @patch("nettacker.core.graph.importlib.import_module", side_effect=ModuleNotFoundError)
-@pytest.mark.xfail(reason="It will hit an UnboundLocalError")
 def test_build_graph_module_not_found(mock_import_module, mock_die_failure):
     build_graph("missing_graph", [])
     mock_die_failure.assert_called_once()
@@ -49,7 +48,6 @@ def test_build_compare_report_success(mock_import_module):
 
 @patch("nettacker.core.graph.die_failure")
 @patch("nettacker.core.graph.importlib.import_module", side_effect=ModuleNotFoundError)
-@pytest.mark.xfail(reason="It will hit an UnboundLocalError")
 def test_build_compare_report_module_not_found(mock_import_module, mock_die_failure):
     build_compare_report({"some": "results"})
     mock_die_failure.assert_called_once()
